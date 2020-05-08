2d8ddfe60a4357a7dcd2e83bdb1cbeb55d81530b
noumankhatib <zestfulnouman@gmail.com>
2020-05-08 16:21:40 +0530

    remove unwanted files

--- server/model/schema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-var Schema =mongoose.Schema;
-const catagory = new Schema({
-    username:String,
-    catagory:Array
-})
-
-module.exports = mongoose.model('catagory', catagory)
