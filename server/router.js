fb5a4059b248923a4333e8c4c113dcb8a9c743ae
noumankhatib <zestfulnouman@gmail.com>
2020-05-17 13:36:50 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -2,89 +2,58 @@ var express = require('express'),
     router = express.Router(),
     dataController = require('./controller/DataController');
 
-var logger = global.logger;
-router.post('/getAllData', function (req, res) {
-    //logger.info('getAllData')
+router.get('/catagory/getAll', function (req, res) {
+   logger.info('getAllData')
     return dataController.getAllData(req, res);
 });
-router.post('/getData', function (req, res) {
-     //logger.info('getData')
-    return dataController.getData(req, res);
+router.get('/catagory/get/:username', function (req, res) {
+    logger.info('getData')
+    return dataController.getData(JSON.parse(req.params.username), res);
 });
 
-router.post('/updateData', function (req, res) {
-   //logger.info('updateData')
+router.put('/catagory/update', function (req, res) {
+  logger.info('updateData')
     return dataController.updateData(req, res);
 });
 
-router.post('/removeData', function (req, res) {
-    //logger.info('removeData')
+router.delete('/catagory/remove', function (req, res) {
+   logger.info('removeData')
     return dataController.removeData(req, res);
 });
 
 
-router.post('/create', function (req, res) {
-  //logger.info('create')
+router.post('/catagory/create', function (req, res) {
+ logger.info('create')
     return dataController.insertData(req.body, res);
 });
 
-
 /**
- * News Feeds
+ * article feeds
  */
 
-router.post('/getAllNewsFeeds', function (req, res) {
-  //logger.info('getAllNewsFeeds')
-  return dataController.getAllNewsFeeds(req, res);
+router.get('/articles/getAllArticles', function (req, res) {
+ logger.info('getAllArticles')
+  return dataController.getAllArticles(req, res);
 });
-router.post('/getNewsFeeds', function (req, res) {
-   //logger.info('getNewsFeeds')
-  return dataController.getNewsFeeds(req, res);
-});
-
-router.post('/updateNewsFeeds', function (req, res) {
- //logger.info('updateNewsFeeds')
-  return dataController.updateNewsFeeds(req, res);
+router.post('/articles/getArticles', function (req, res) {
+  logger.info('getArticles')
+  return dataController.getArticles(req, res);
 });
 
-router.post('/removeNewsFeeds', function (req, res) {
-  //logger.info('removeNewsFeeds')
-  return dataController.removeNewsFeeds(req, res);
+router.put('/articles/updatesArticles', function (req, res) {
+logger.info('updatesArticles')
+  return dataController.updateArticles(req, res);
 });
 
-
-router.post('/insertNewsFeeds', function (req, res) {
-//logger.info('insertNewsFeeds')
-  return dataController.insertNewsFeeds(req.body, res);
-});
-
-/**
- * Opinion Feeds
- */
-
-router.post('/getAllOpinionFeeds', function (req, res) {
-  //logger.info('getAllOpinionFeeds')
-  return dataController.getAllOpinionFeeds(req, res);
-});
-router.post('/getOpinionFeeds', function (req, res) {
-   //logger.info('getOpinionFeeds')
-  return dataController.getOpinionFeeds(req, res);
-});
-
-router.post('/updateOpinionFeeds', function (req, res) {
- //logger.info('updateOpinionFeeds')
-  return dataController.updateOpinionFeeds(req, res);
-});
-
-router.post('/removeOpinionFeeds', function (req, res) {
-  //logger.info('removeOpinionFeeds')
-  return dataController.removeOpinionFeeds(req, res);
+router.delete('/articles/removeArticles', function (req, res) {
+ logger.info('removeArticles')
+  return dataController.removeArticles(req, res);
 });
 
 
-router.post('/insertOpinionFeeds', function (req, res) {
-//logger.info('insertOpinionFeeds')
-  return dataController.insertOpinionFeeds(req.body, res);
+router.post('/articles/createArticles', function (req, res) {
+logger.info('insertArticles')
+  return dataController.insertArticles(req.body, res);
 });
 
 
