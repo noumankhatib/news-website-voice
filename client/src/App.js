2a81cec88740c4cd4c7681044b629a50c4a703f9
Sumeet Pol <polsumeet3666@gmail.com>
2020-05-22 10:47:14 +0530

    added card deck

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,119 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
+import "./App.css";
+import { CardDeck, Card } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.data = [
+			{
+				_id: "5ebfe27a3d73c057ccbdc4db",
+				catagory: "politic",
+				title: "recent fight 1",
+				articletype: "opinionFeeds",
+				author: "nouman",
+				content: "Welcome to the world",
+				url: "www.facebook.com",
+				v: 0,
+			},
+			{
+				_id: "5ebfe27c3d73c057ccbdc4dc",
+				catagory: "politic",
+				title: "recent fight 2",
+				articletype: "opinionFeeds",
+				author: "nouman",
+				content: "Welcome to the world",
+				url: "www.facebook.com",
+				_v: 0,
+			},
+			{
+				_id: "5ebfe27c3d73c057ccbdc4dc",
+				catagory: "politic",
+				title: "recent fight 2",
+				articletype: "opinionFeeds",
+				author: "nouman",
+				content: "Welcome to the world",
+				url: "www.facebook.com",
+				_v: 0,
+			}
+		];
+	}
+	render() {
+		let cards = this.data.map((d, index) => {
+			console.log(d, index);
+			// eslint-disable-next-line no-unused-expressions
+			return (
+				<Card>
+					<Card.Img variant="top" src="holder.js/100px160" />
+					<Card.Body>
+						<Card.Title>{d.title}</Card.Title>
+						<Card.Text>{d.content}</Card.Text>
+					</Card.Body>
+					<Card.Footer>
+						<small className="text-muted">Author: {d.author}</small>
+					</Card.Footer>
+				</Card>
+			);
+		});
+
+		return (
+			<div className="App">
+				<CardDeck>{cards}</CardDeck>
+
+				{/* <CardDeck>
+					<Card>
+						<Card.Img variant="top" src="holder.js/100px160" />
+						<Card.Body>
+							<Card.Title>Card title</Card.Title>
+							<Card.Text>
+								This is a wider card with supporting text below
+								as a natural lead-in to additional content. This
+								content is a little bit longer.
+							</Card.Text>
+						</Card.Body>
+						<Card.Footer>
+							<small className="text-muted">
+								Last updated 3 mins ago
+							</small>
+						</Card.Footer>
+					</Card>
+					<Card>
+						<Card.Img variant="top" src="holder.js/100px160" />
+						<Card.Body>
+							<Card.Title>Card title</Card.Title>
+							<Card.Text>
+								This card has supporting text below as a natural
+								lead-in to additional content.{" "}
+							</Card.Text>
+						</Card.Body>
+						<Card.Footer>
+							<small className="text-muted">
+								Last updated 3 mins ago
+							</small>
+						</Card.Footer>
+					</Card>
+					<Card>
+						<Card.Img variant="top" src="holder.js/100px160" />
+						<Card.Body>
+							<Card.Title>Card title</Card.Title>
+							<Card.Text>
+								This is a wider card with supporting text below
+								as a natural lead-in to additional content. This
+								card has even longer content than the first to
+								show that equal height action.
+							</Card.Text>
+						</Card.Body>
+						<Card.Footer>
+							<small className="text-muted">
+								Last updated 3 mins ago
+							</small>
+						</Card.Footer>
+					</Card>
+				</CardDeck> */}
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/components/articles/home.js
+++ client/src/components/articles/home.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import {getAllArticles} from '../utils/https-client'
-import  NavBar  from '../navbar/navbar';
-import {CardGroup,Card} from 'react-bootstrap';
+import React, { Component } from "react";
+import { getAllArticles } from "../utils/https-client";
+import NavBar from "../navbar/navbar";
+import { CardGroup } from "react-bootstrap";
 //import { Container, Row, Col, Alert, Form } from 'react-bootstrap';
 // import DatePicker from 'react-datepicker';
 // import "react-datepicker/dist/react-datepicker.css";
@@ -9,50 +9,49 @@ import {CardGroup,Card} from 'react-bootstrap';
 // import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        isLoading: true,
-        articles: [],
-        error: null
-      }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoading: true,
+			articles: [],
+			error: null,
+		};
 
-        getAllArticles().then(result=>{
-            this.setState({
-                articles: result,
-                isLoading: false,
-              })
-              
-        }).catch((error)=>{
-            this.setState({ error, isLoading: false })
-        })
+		getAllArticles()
+			.then((result) => {
+				this.setState({
+					articles: result,
+					isLoading: false,
+				});
+			})
+			.catch((error) => {
+				this.setState({ error, isLoading: false });
+			});
+	}
 
-      
-  }
-  
-      render() {
-        const { articles,isLoading } = this.state;
-        console.log("articles"+JSON.stringify(articles))
-        return (
-        <React.Fragment>
-           <div>
-              <NavBar/>
-          </div>
-           <h1>React Fetch - Blog</h1>
-   
-           {!isLoading ? (
-          articles.data.map(article => {
-            return(
-              <div>
-                <CardGroup></CardGroup>
-                     
-            </div>
-           ) })
-          ): (<p>Loading...</p>)
-      }
-      </React.Fragment>
-        )
-    }
-      
-  }
+	render() {
+		const { articles, isLoading } = this.state;
+		console.log("articles" + JSON.stringify(articles));
+		return (
+			<React.Fragment>
+				<div>
+					<NavBar />
+				</div>
+				<h1>React Fetch - Blog</h1>
+
+				{!isLoading ? (
+					articles.data.map((article) => {
+						return (
+							<div>
+								<CardGroup></CardGroup>
+							</div>
+						);
+					})
+				) : (
+					<p>Loading...</p>
+				)}
+			</React.Fragment>
+		);
+	}
+}
 export default Home;
