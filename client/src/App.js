4daa04840aead6d4b4738f5a81a17f376d55c733
Sumeet Pol <polsumeet3666@gmail.com>
2020-05-22 10:48:34 +0530

    added card deck

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ class App extends React.Component {
 				content: "Welcome to the world",
 				url: "www.facebook.com",
 				_v: 0,
-			}
+			},
 		];
 	}
 	render() {
@@ -60,57 +60,6 @@ class App extends React.Component {
 		return (
 			<div className="App">
 				<CardDeck>{cards}</CardDeck>
-
-				{/* <CardDeck>
-					<Card>
-						<Card.Img variant="top" src="holder.js/100px160" />
-						<Card.Body>
-							<Card.Title>Card title</Card.Title>
-							<Card.Text>
-								This is a wider card with supporting text below
-								as a natural lead-in to additional content. This
-								content is a little bit longer.
-							</Card.Text>
-						</Card.Body>
-						<Card.Footer>
-							<small className="text-muted">
-								Last updated 3 mins ago
-							</small>
-						</Card.Footer>
-					</Card>
-					<Card>
-						<Card.Img variant="top" src="holder.js/100px160" />
-						<Card.Body>
-							<Card.Title>Card title</Card.Title>
-							<Card.Text>
-								This card has supporting text below as a natural
-								lead-in to additional content.{" "}
-							</Card.Text>
-						</Card.Body>
-						<Card.Footer>
-							<small className="text-muted">
-								Last updated 3 mins ago
-							</small>
-						</Card.Footer>
-					</Card>
-					<Card>
-						<Card.Img variant="top" src="holder.js/100px160" />
-						<Card.Body>
-							<Card.Title>Card title</Card.Title>
-							<Card.Text>
-								This is a wider card with supporting text below
-								as a natural lead-in to additional content. This
-								card has even longer content than the first to
-								show that equal height action.
-							</Card.Text>
-						</Card.Body>
-						<Card.Footer>
-							<small className="text-muted">
-								Last updated 3 mins ago
-							</small>
-						</Card.Footer>
-					</Card>
-				</CardDeck> */}
 			</div>
 		);
 	}
